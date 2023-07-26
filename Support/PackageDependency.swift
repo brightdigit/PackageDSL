df45473385fd6b29a2e7caed6b7b713490e50fdd
Leo Dion <leogdion@brightdigit.com>
2023-07-25 20:42:52 -0400

    fixed using multiple products from same dependency

--- Support/PackageDependency.swift
+++ Support/PackageDependency.swift
@@ -7,12 +7,26 @@
 import PackageDescription
 
 protocol PackageDependency: Dependency {
+  var packageName: String { get }
   var dependency: _PackageDescription_PackageDependency { get }
 }
 
 extension PackageDependency {
   var productName: String {
     "\(Self.self)"
+  }
+  
+  var packageName : String {
+    switch self.dependency.kind {
+    case let .sourceControl(name: name, location: location, requirement: _):
+      return name ?? location.packageName ?? productName
+    case let .fileSystem(name: name, path: path):
+      return name ?? path.packageName ?? productName
+    case let .registry(id: id, requirement: _):
+      return id
+    @unknown default:
+      return productName
+    }
   }
 
   var targetDepenency: _PackageDescription_TargetDependency {
