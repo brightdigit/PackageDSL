f41f45befe405966797ab4f1af25aa1146a6b997
Leo Dion <leogdion@brightdigit.com>
2024-03-29 21:25:34 -0400

    fixing Package Dependency

--- Support/PackageDependency.swift
+++ Support/PackageDependency.swift
@@ -1,7 +1,6 @@
 //
 // PackageDependency.swift
-// Copyright (c) 2023 BrightDigit.
-// Licensed under MIT License
+// Copyright (c) 2024 BrightDigit.
 //
 
 import PackageDescription
@@ -15,13 +14,15 @@ extension PackageDependency {
   var productName: String {
     "\(Self.self)"
   }
-  
-  var packageName : String {
+
+  var packageName: String {
     switch self.dependency.kind {
     case let .sourceControl(name: name, location: location, requirement: _):
       return name ?? location.packageName ?? productName
+
     case let .fileSystem(name: name, path: path):
       return name ?? path.packageName ?? productName
+
     case let .registry(id: id, requirement: _):
       return id
     @unknown default:
@@ -34,7 +35,14 @@ extension PackageDependency {
     case let .sourceControl(name: name, location: location, requirement: _):
       let packageName = name ?? location.packageName
       return .product(name: productName, package: packageName)
-
+    case let .fileSystem(name: name, path: path):
+      if let packageName = name ?? path.components(separatedBy: "/").last {
+        return .product(name: productName, package: packageName)
+      } else {
+        return .byName(name: productName)
+      }
+    case .registry:
+      return .byName(name: productName)
     @unknown default:
       return .byName(name: productName)
     }
