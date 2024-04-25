744139392795d759d7cce490671d1fd38872a420
Leo Dion <leogdion@brightdigit.com>
2024-04-24 20:52:20 -0400

    adding upcoming features

--- experimentalFeatures.sh
+++ experimentalFeatures.sh
@@ -11,6 +11,9 @@ api_url="https://api.github.com/repos/$repo_owner/$repo_name/contents/$directory
 # Personal access token for GitHub authentication
 access_token="$GITHUB_ACCESS_TOKEN"
 
+# Proposal number to start searching from
+start_proposal=363
+
 # Check if the access token environment variable is set
 if [ -z "$access_token" ]; then
 	echo "Error: GitHub access token is not set."
@@ -34,14 +37,32 @@ for row in $(echo "$response" | jq -r '.[] | @base64'); do
 	# Parse JSON object for each file
 	file=$(echo "$row" | base64 -d)
 
-	# Extract file name and download URL
+	# Extract file name and proposal number
 	file_name=$(echo "$file" | jq -r '.name')
+	proposal_number=$(echo "$file_name" | cut -d'-' -f1)
+	
+	# Skip files before the start proposal
+	if [ "$proposal_number" -lt "$start_proposal" ]; then
+		continue
+	fi
+
 	download_url=$(echo "$file" | jq -r '.download_url')
 
 	# Download the content of the file
 	file_content=$(curl -s "$download_url")
 
 	# Search for strings preceded by "-enable-experimental-feature" and print the matches
-	echo "Searching in $file_name:"
-	echo "$file_content" | grep -oE '\-enable-experimental-feature\s+[[:alnum:]]{2,}' | awk '{print $2}'
+	experimental_features=$(echo "$file_content" | grep -oE '\-enable-experimental-feature\s+[[:alnum:]]{2,}' | awk '{print $2}')
+
+	# If no experimental features found, search for upcoming features
+	if [ -z "$experimental_features" ]; then
+		upcoming_features=$(echo "$file_content" | awk -F ": +" '/Upcoming Feature Flag:/ && $2 ~ /^`[[:alnum:]]+`$/ {print substr($2, 2, length($2)-2)}')
+		if [ -n "$upcoming_features" ]; then
+			echo "Upcoming features in $file_name:"
+			echo "$upcoming_features"
+		fi
+	else
+		echo "Experimental features in $file_name:"
+		echo "$experimental_features"
+	fi
 done
