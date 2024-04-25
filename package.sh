e5c43aa9c6a1c97a920ce515c0cbada373e32220
Leo Dion <leogdion@brightdigit.com>
2024-04-25 13:38:49 -0400

    fixing package script

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "// swift-tools-version: 5.9" > Package.swift
-cat Support/**/*.swift >> Package.swift
+find Support -name '*.swift' -type f -exec cat {} + >> Package.swift
 cat Sources/**/*.swift >> Package.swift
 cat Sources/*.swift >> Package.swift
