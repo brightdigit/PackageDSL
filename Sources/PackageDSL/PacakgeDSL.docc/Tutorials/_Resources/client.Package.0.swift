e433dd6239d87df22854ec39134533465fc9c75b
Leo Dion <leogdion@brightdigit.com>
2025-01-04 14:56:50 -0500

    working through tutorial

---
+++ Sources/PackageDSL/PacakgeDSL.docc/Tutorials/_Resources/client.Package.0.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "GreetingServiceClient",
+    targets: [
+        .executableTarget(
+            name: "GreetingServiceClient"
+        )
+    ]
+)
